a5a0e6dfe899ce889735f8f514057f03513d4c6b
peijian.wang <51995464@qq.com>
2018-02-13 16:16:41 +0800

    增加微信服务号的数据库表

--- db/todo.sql
+++ db/todo.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : 本地
-Source Server Version : 50710
-Source Host           : localhost:3306
+Source Server Version : 50717
+Source Host           : 127.0.0.1:3306
 Source Database       : todo
 
 Target Server Type    : MYSQL
-Target Server Version : 50710
+Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2018-02-04 22:01:52
+Date: 2018-02-13 16:15:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -62,3 +62,44 @@ CREATE TABLE `c_user` (
 -- Records of c_user
 -- ----------------------------
 INSERT INTO `c_user` VALUES ('1', '1', '2018-02-03 23:48:40', '1', '1');
+
+-- ----------------------------
+-- Table structure for wx_token
+-- ----------------------------
+DROP TABLE IF EXISTS `wx_token`;
+CREATE TABLE `wx_token` (
+  `appid` varchar(255) NOT NULL,
+  `accessToken` varchar(255) NOT NULL,
+  `expiresTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `type` char(20) NOT NULL,
+  PRIMARY KEY (`appid`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of wx_token
+-- ----------------------------
+INSERT INTO `wx_token` VALUES ('wxa1b636191d0d98f2', '6_fVzpzsiKkimE4-_hvU6QD-uE61LkB4rWgH0ymAqS6TGN3Jxhym0w6uCwp2peZ7RTvzlMlEkbP3z862qH8REIeOKPOWgZ4nFMWgByXKL577X_WTC7sXESshSUZ7wDFWcAJATVM', '2018-02-13 15:00:11', 'ACCOUNT_TOKEN');
+INSERT INTO `wx_token` VALUES ('wxa1b636191d0d98f2', 'HoagFKDcsGMVCIY2vOjf9hzH6yFWfkLUkwcMUsYOYm3Jqf9WbI78iS3r935CD2tj-OsYbxI6sYwXFov2FmMhpw', '2018-02-13 15:00:12', 'JS_SDK_TICKET');
+
+-- ----------------------------
+-- Table structure for wx_user
+-- ----------------------------
+DROP TABLE IF EXISTS `wx_user`;
+CREATE TABLE `wx_user` (
+  `openid` varchar(255) NOT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `sex` varchar(255) DEFAULT NULL,
+  `province` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `headimgurl` varchar(255) DEFAULT NULL,
+  `privilege` varchar(255) DEFAULT NULL,
+  `unionid` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`openid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of wx_user
+-- ----------------------------
+INSERT INTO `wx_user` VALUES ('ozEfP0bNUNC4NUrsi-7Zsuk6AnW8', '不行 我太冷', '2', '北京', '朝阳', '中国', 'http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTL62BxLqChAXox2XrFqnMTDyk7nJosic9MUdTX3FbZsictx3WkbS6g25h8zo1oUzCKSPty1WBlMfaoA/132', '', null);
+INSERT INTO `wx_user` VALUES ('ozEfP0cCn_Xyl5LDxPMIuFZOeroU', '王佩剑', '1', '北京', '西城', '中国', 'http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJHo8fqDsTr8yPPicoaOFpIssY1kxr3p9iabEdsLpM8n0qJVzYn8FHZdkEo6ybYLtsIga6xoRyl17GA/132', '', null);
