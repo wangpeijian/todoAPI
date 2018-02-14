87494efce6860e8015723be446291dc4e32d5545
peijian.wang <51995464@qq.com>
2018-02-14 10:44:05 +0800

    增加微信服务号的数据库表

--- src/core/handle.mjs
+++ src/core/handle.mjs
@@ -245,8 +245,15 @@ function notFound(response) {
     result.code = 404;
     result.msg = 'request not found';
 
-    response.writeHead(404, {'Content-Type': 'application/json'});
-    response.write(util.inspect(result));
+    $log.i(`响应结果 404`);
+    response.writeHead(200, {
+        'Content-Type': "application/json;charset=utf-8",
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With',
+        'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+        'X-Powered-By': '3.2.1',
+    });
+    response.write(JSON.stringify(result));
     response.end();
 }
 
